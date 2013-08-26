52c1806552277d6510e95302f2d5934917814994
Nick Harley <nickharley84@gmail.com>
2013-08-26 12:59:25 +1200

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bedspring::Application.config.secret_key_base = '9165b0035384506b8ffa4b30fadd79b35aee47ddae478ac0ee5fa961a893c775f4c82411753b92cb97d7dada90a18916241297abd5cf60ca6b7e608163833b4d'
